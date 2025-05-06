49e3a2eb97a1efc895124999f830039a8330d488
Vishnu0719 <bajjurivishnu@ufl.edu>
2025-05-06 12:58:00 -0400

    include file

---
+++ include.v
@@ -0,0 +1,11 @@
+
+
+`define numlayers 4
+`define data_width 31
+`define layer1_neurons 30
+`define layer2_neurons 30
+`define layer3_neurons 10
+
+`define layer1_weights 784
+`define layer2_weights 30
+`define layer3_weights 30
